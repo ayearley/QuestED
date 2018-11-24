7c2132fefc28a6f431e4ef5d509e321a003de37e
Alex Yearley <alex.yearley@me.com>
2018-11-24 10:51:39 -0500

    Initial Commit

---
+++ Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Alex Yearley on 11/24/18.
+//  Copyright © 2018 Alex Yearley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
