e47b815607019f6202d1b7f0eaf6970fdd167f21
Alex Yearley <alex.yearley@me.com>
2018-12-01 14:39:59 -0500

    the buttons kinda work

--- Prototype/MapViewController.swift
+++ Prototype/MapViewController.swift
@@ -19,19 +19,19 @@ class MapViewController: UIViewController {
     //Load all of these from file instead of hard coding
     var buttonWidth = 50;
     var buttonHeight = 50;
-    var level1x = 45;
-    var level1y = 220;
+    //var level1x = 45;
+    //var level1y = 220;
     //var level2x = 245;
     //var level2y = 350;
-    var level3x = 120;
-    var level3y = 545;
+    //var level3x = 120;
+    //var level3y = 545;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         Bundle.main.loadNibNamed("MapViewController", owner: self, options: nil)
         
         let buttonLevel1 = UIButton(type: .custom)
-        buttonLevel1.frame = CGRect(x: Double(screenSize.width)/7, y: Double(screenSize.height)/9, width: Double(buttonWidth), height: Double(buttonHeight))
+        buttonLevel1.frame = CGRect(x: Double(screenSize.width)/7.35, y: Double(screenSize.height)/4.3, width: Double(buttonWidth), height: Double(buttonHeight))
         buttonLevel1.layer.cornerRadius = 0.5*buttonLevel1.bounds.size.width
         buttonLevel1.clipsToBounds = true
         buttonLevel1.setImage(UIImage(named:"easyButton.png"), for: .normal)
@@ -39,7 +39,7 @@ class MapViewController: UIViewController {
         view.addSubview(buttonLevel1)
         
         let buttonLevel2 = UIButton(type: .custom)
-        buttonLevel2.frame = CGRect(x: Double(screenSize.width)/1.25, y: Double(screenSize.height)/2, width: Double(buttonWidth), height: Double(buttonHeight))
+        buttonLevel2.frame = CGRect(x: Double(screenSize.width)/1.63, y: Double(screenSize.height)/2.48, width: Double(buttonWidth), height: Double(buttonHeight))
         buttonLevel2.layer.cornerRadius = 0.5*buttonLevel2.bounds.size.width
         buttonLevel2.clipsToBounds = true
         buttonLevel2.setImage(UIImage(named:"easyButton.png"), for: .normal)
@@ -47,7 +47,7 @@ class MapViewController: UIViewController {
         view.addSubview(buttonLevel2)
         
         let buttonLevel3 = UIButton(type: .custom)
-        buttonLevel3.frame = CGRect(x: Double(screenSize.width)/5, y: Double(screenSize.height)/1.25, width: Double(buttonWidth), height: Double(buttonHeight))
+        buttonLevel3.frame = CGRect(x: Double(screenSize.width)/4.42, y: Double(screenSize.height)/1.65, width: Double(buttonWidth), height: Double(buttonHeight))
         buttonLevel3.layer.cornerRadius = 0.5*buttonLevel3.bounds.size.width
         buttonLevel3.clipsToBounds = true
         buttonLevel3.setImage(UIImage(named:"easyButton.png"), for: .normal)
