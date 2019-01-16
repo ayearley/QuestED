822ba57e0fe2c467040d3e28977e663f1f48f8b2
Jacob Zeitlin <jz197@duke.edu>
2019-01-15 21:11:56 -0500

    Reading level status

--- Prototype/AppDelegate.swift
+++ Prototype/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var navController: UINavigationController?
+    
+    var levelStatus = [Int]()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
