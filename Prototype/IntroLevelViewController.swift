e4ec9b53cc149c9135add815594b53b0b44ab13a
Jacob Zeitlin <jz197@duke.edu>
2019-02-03 15:57:32 -0500

    Intro

--- Prototype/IntroLevelViewController.swift
+++ Prototype/IntroLevelViewController.swift
@@ -30,10 +30,23 @@ class IntroLevelViewController: UIViewController {
     
     var doctorImage: UIImageView = UIImageView()
     var doctorLabel: UILabel = UILabel()
+    var bubbleImage: UIImageView = UIImageView()
+    var doctorLines: [String] = [String]()
+    var totalDoctorLines = 0
+    var currentLineD = 0
+
     var state = 0
+    var nurseSpeaks: Bool = false
     
     var nurseImage: UIImageView = UIImageView()
     var nurseLabel: UILabel = UILabel()
+    var nurseBubbleImage: UIImageView = UIImageView()
+    var nurseLines: [String] = [String]()
+    var totalNurseLines = 0
+    var currentLineN = 0
+    
+    var introText: String
+    var nurseText: String
     
     init(runner: LevelRunner) {
         self.quiz = false;
@@ -42,6 +55,8 @@ class IntroLevelViewController: UIViewController {
         textFile = runner.levelText
         self.videoName = ""
         print("Text file: \(textFile)")
+        introText = ""
+        nurseText = ""
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -51,6 +66,8 @@ class IntroLevelViewController: UIViewController {
         self.textFile = ""
         self.videoName = ""
         self.levelRunner = LevelRunner(textIn: self.textFile)
+        introText = ""
+        nurseText = ""
         super.init(coder: aDecoder)
     }
     
@@ -99,57 +116,184 @@ class IntroLevelViewController: UIViewController {
     
     func createDoctor() {
         doctorImage = UIImageView(image: UIImage(imageLiteralResourceName: "doctor.png"))
-        doctorImage.frame = CGRect(x: Double(screenSize.width) * 0.1, y: Double(screenSize.height * 0.4), width: Double(screenSize.height) * 0.2, height: Double(screenSize.height) * 0.27)
+        doctorImage.frame = CGRect(x: Double(screenSize.width) * 0.1, y: Double(screenSize.height * 0.2), width: Double(screenSize.height) * 0.2, height: Double(screenSize.height) * 0.27)
         view.addSubview(doctorImage)
         doctorImage.isHidden = true
         
+        bubbleImage = UIImageView(image: UIImage(imageLiteralResourceName: "speechbubble_map.png"))
+        
         doctorLabel = UILabel(frame: CGRect(x: Double(screenSize.width) * 0.22, y: Double(screenSize.height) * 0.26, width: Double(screenSize.width) * 0.26, height: Double(screenSize.height) * 0.24))
-        doctorLabel.backgroundColor = UIColor.white
+        
+        bubbleImage.frame = CGRect(x: doctorLabel.frame.minX * 0.9, y: doctorLabel.frame.minY, width: doctorLabel.frame.width * 1.15, height: doctorLabel.frame.height)
+        
+        view.addSubview(bubbleImage)
+        bubbleImage.isHidden = true
+        
+        doctorLabel.backgroundColor = UIColor.clear
+        doctorLabel.numberOfLines = 4
+        doctorLabel.lineBreakMode = .byWordWrapping
         view.addSubview(doctorLabel)
         doctorLabel.isHidden = true
     }
     
     func createNurse() {
-        nurseImage = UIImageView(image: UIImage(imageLiteralResourceName: "horse.jpg"))
-        nurseImage.frame = CGRect(x: Double(screenSize.width) * 0.8, y: Double(screenSize.height * 0.4), width: Double(screenSize.height) * 0.3, height: Double(screenSize.height) * 0.27)
-        view.addSubview(nurseImage)
-        nurseImage.isHidden = true
-        
         nurseLabel = UILabel(frame: CGRect(x: Double(screenSize.width) * 0.5, y: Double(screenSize.height) * 0.26, width: Double(screenSize.width) * 0.26, height: Double(screenSize.height) * 0.24))
         nurseLabel.backgroundColor = UIColor.white
         view.addSubview(nurseLabel)
         nurseLabel.isHidden = true
+        
+        nurseImage = UIImageView(image: UIImage(imageLiteralResourceName: "horse.jpg"))
+        nurseImage.frame = CGRect(x: Double(screenSize.width) * 0.8, y: Double(screenSize.height * 0.2), width: Double(screenSize.height) * 0.2, height: Double(screenSize.height) * 0.27)
+        view.addSubview(nurseImage)
+        nurseImage.isHidden = true
+        
+        nurseBubbleImage = UIImageView(image: UIImage(imageLiteralResourceName: "speechbubble_large.png"))
+        
+        nurseLabel = UILabel(frame: CGRect(x: Double(screenSize.width) * 0.52, y: Double(screenSize.height) * 0.26, width: Double(screenSize.width) * 0.26, height: Double(screenSize.height) * 0.24))
+        
+        nurseBubbleImage.frame = CGRect(x: nurseLabel.frame.minX * 0.9, y: doctorLabel.frame.minY, width: doctorLabel.frame.width * 1.15, height: doctorLabel.frame.height)
+        
+        view.addSubview(nurseBubbleImage)
+        nurseBubbleImage.isHidden = true
+        
+        nurseLabel.backgroundColor = UIColor.clear
+        nurseLabel.numberOfLines = 4
+        nurseLabel.lineBreakMode = .byWordWrapping
+        view.addSubview(nurseLabel)
+        nurseLabel.isHidden = true
+    }
+    
+    func readTextFile() {
+        if let filepath = Bundle.main.path(forResource: textFile, ofType: "txt") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                
+                let doctorRange = contents.range(of: "doc: ")
+                let endIntroRange = contents.range(of: "*END INTRO*")
+                
+                // print("Pre: \(preIntroRange), end: \(endPreIntroRange)")
+                
+                introText = String(contents[doctorRange!.upperBound..<endIntroRange!.lowerBound])
+                
+                if (introText.contains("nurse: ")) {
+                    nurseSpeaks = true
+                    
+                    let nurseRange = contents.range(of: "nurse: ")
+                    introText = String(contents[doctorRange!.upperBound..<nurseRange!.lowerBound])
+                    nurseText = String(contents[nurseRange!.upperBound..<endIntroRange!.lowerBound])
+                }
+            } catch {
+                debugPrint("contents of text file could not be loaded")
+            }
+        } else {
+            debugPrint("text file not found")
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         switch state {
         case 0:
+            print("Intro text: \(introText)")
+            
+            doctorLabel.text = introText
+            
+            doctorLines = getLinesArrayOfString(in: doctorLabel)
+            
+            totalDoctorLines = doctorLines.count
+            
+            
+            if (doctorLines.count == 3) {
+                doctorLabel.text = doctorLines[0] + doctorLines[1] + doctorLines[2]
+            } else if (doctorLines.count == 2) {
+                doctorLabel.text = doctorLines[0] + doctorLines[1]
+            } else if (doctorLines.count == 1) {
+                doctorLabel.text = doctorLines[0]
+            } else {
+                doctorLabel.text = doctorLines[0] + doctorLines[1] + doctorLines[2] + doctorLines[3]
+            }
+            
             doctorImage.isHidden = false
             doctorLabel.isHidden = false
+            bubbleImage.isHidden = false
+            
+            state = 1
         case 1:
-            doctorImage.isHidden = true
-            doctorLabel.isHidden = true
-            nurseImage.isHidden = false
-            nurseLabel.isHidden = false
+            currentLineD += 4
+            
+            
+            if (currentLineD + 3 < totalDoctorLines) {
+                var newText: String = ""
+                
+                for  i in currentLineD..<totalDoctorLines {
+                    if (i <= currentLineD + 3) {
+                        newText += doctorLines[i]
+                    }
+                }
+                
+                doctorLabel.text = newText
+            } else {
+                
+                if (!nurseSpeaks) {
+                    view.addSubview(continueButton)
+                } else {
+                    nurseLabel.text = nurseText
+                    
+                    nurseLines = getLinesArrayOfString(in: nurseLabel)
+                    
+                    totalNurseLines = nurseLines.count
+                    
+                    if (nurseLines.count == 3) {
+                        nurseLabel.text = nurseLines[0] + nurseLines[1] + nurseLines[2]
+                    } else if (nurseLines.count == 2) {
+                        nurseLabel.text = nurseLines[0] + nurseLines[1]
+                    } else if (nurseLines.count == 1) {
+                        nurseLabel.text = nurseLines[0]
+                    } else {
+                        doctorLabel.text = nurseLines[0] + nurseLines[1] + nurseLines[2] + nurseLines[3]
+                    }
+                    
+                    doctorImage.isHidden = true
+                    doctorLabel.isHidden = true
+                    bubbleImage.isHidden = true
+                    nurseBubbleImage.isHidden = false
+                    nurseImage.isHidden = false
+                    nurseLabel.isHidden = false
+                }
+                
+                state = 2
+            }
+            
         case 2:
-            doctorImage.isHidden = false
-            doctorLabel.isHidden = false
-            nurseImage.isHidden = true
-            nurseLabel.isHidden = true
-        case 3:
-            doctorImage.isHidden = true
-            doctorLabel.isHidden = true
-        case 4:
-            view.addSubview(continueButton)
+            
+            currentLineN += 4
+            
+            
+            if (currentLineN + 3 < totalNurseLines) {
+                var newText: String = ""
+                
+                for  i in currentLineN..<totalNurseLines {
+                    if (i <= currentLineN + 3) {
+                        newText += nurseLines[i]
+                    }
+                }
+                
+                nurseLabel.text = newText
+            } else {
+                
+                if (nurseSpeaks) {
+                    view.addSubview(continueButton)
+                }
+                
+                state = 3
+            }
+            
             
         default:
             doctorImage.isHidden = doctorImage.isHidden
         }
-        
-        state += 1
     }
     
-    func readTextFile() {
+    /*func readTextFile() {
         if let filepath = Bundle.main.path(forResource: textFile, ofType: "txt") {
             do {
                 let contents = try String(contentsOfFile: filepath)
@@ -184,7 +328,7 @@ class IntroLevelViewController: UIViewController {
         } else {
             debugPrint("text file not found")
         }
-    }
+    }*/
     
     func getLinesArrayOfString(in label: UILabel) -> [String] {
         
